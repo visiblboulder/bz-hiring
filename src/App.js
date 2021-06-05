208ee7b2b2383211b5d5bc6f352bedb9dce5479a
Emily J <emi.jowers@gmail.com>
2021-06-05 08:33:31 -0400

    Working Page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import movies from './data/movies.json';
+
+const movie_cards = movies.map( (movie) => {
+  return (
+  <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <img className="w-full" src={`/img/${movie.poster}`} alt="Movie Poster" />
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2 text-center uppercase">{ movie.title }</div>
+      <p className="text-gray-500 text-base text-center uppercase font-bold ">{ movie.tag_line }</p>
+      <p className="text-gray-700 text-base">{ movie.description }</p>
+    </div>
+    <hr />
+    <div className="grid grid-cols-2">
+      <div><img src="/img/star-full.png" alt="Star" className="inline-block w-5 mx-2 mb-1" />{ movie.star_rating }</div>
+      <div className="text-right"><img src="/img/star-full.png" alt="Star" className="inline-block w-5 mx-2 my-1" />{ movie.release_date }</div>
+    </div>
+    <hr />
+    <div className="px-6 pt-4 pb-2 bg-gray-100"> {/* Genre */}
+      { movie.genres.map((genre, i) => (
+        <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{ genre }</span>
+      ))}
+    </div>
+  </div>
+  );
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex-auto ">
+      <div className="overflow-hidden">
+        <div className="grid grid-cols-3 p-10 gap-4">
+          { movie_cards }
+        </div>
+      </div>
+    </main>
   );
 }
 
